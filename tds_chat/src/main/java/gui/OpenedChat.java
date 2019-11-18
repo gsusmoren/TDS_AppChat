7a4c9a6bfc7f1f263ac7f6a6111bf394ae711f76
gsusmoren <jesusmorenogutierrez@gmail.com>
2019-11-18 18:25:58 +0100

    Realizando el Chat Selccionado

--- tds_chat/src/main/java/gui/OpenedChat.java
+++ tds_chat/src/main/java/gui/OpenedChat.java
@@ -1,6 +1,13 @@
+/*
+ * Esta Clase representa graficamente un chat abierto con cierto contacto 
+ * en el lado izquierdo de la pantalla principal
+ * 
+ */
 package gui;
+import java.awt.BorderLayout;
 import java.awt.Color;
 import java.awt.Dimension;
+import java.awt.Image;
 import java.time.LocalDate;
 
 import javax.swing.Box;
@@ -23,26 +30,41 @@ public class OpenedChat extends JPanel{
 		name =  nm;
 		ultMsg = ult;
 		this.setLayout(new BoxLayout(this, BoxLayout.X_AXIS));
-
-	
+		Image im = icono.getImage();
+		Image scaled = im.getScaledInstance(60, 60, java.awt.Image.SCALE_SMOOTH);
+		icono = new ImageIcon(scaled);
 		JLabel icon =new  JLabel(icono);	
 		add(icon);
-		
 		JPanel pRight = new JPanel();
 		pRight.setLayout(new BoxLayout(pRight, BoxLayout.Y_AXIS));
+		
+
 		add(pRight);
 		
 		JPanel pRightTop = new JPanel();
 		pRightTop.setLayout(new BoxLayout(pRightTop,BoxLayout.X_AXIS));
+		pRightTop.add(Box.createHorizontalGlue());
 		pRight.add(pRightTop);
+		JPanel pRightBot = new JPanel();
+		pRight.add(pRightBot);
 		
 		JLabel nameL = new JLabel(name);
 		JLabel dateL = new JLabel(LocalDate.now().toString());
 		JLabel last = new JLabel(ultMsg);
+		nameL.setMaximumSize(new Dimension(80,20));
+	
+		//dateL.setMinimumSize(new Dimension(100,10));
 		pRightTop.add(nameL);
-		pRightTop.add(Box.createRigidArea(new Dimension(60, 20)));
+		pRightTop.add(Box.createRigidArea(new Dimension(70, 10)));
 		pRightTop.add(dateL);
-		pRight.add(last);
+		last.setMaximumSize(new Dimension(210, 20));
+		last.setPreferredSize(new Dimension(210, 20));
+		pRightBot.add(last);
+		
+	
+
+		
+		//this.add(Box.createRigidArea(new Dimension(60, 20)));
 		
 		setBackground(Color.red);
 	}
---
+++ tds_chat/src/main/java/gui/SelectedChat.java
@@ -0,0 +1,13 @@
+package gui;
+
+import javax.swing.JPanel;
+import javax.swing.JScrollPane;
+
+public class SelectedChat extends JScrollPane{
+	private String emisor;
+	
+	public SelectedChat(String em) {
+		emisor = em;
+	}
+
+}
