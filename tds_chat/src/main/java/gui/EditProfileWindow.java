cfa8c0a3f72e75eb32758ad32dc8be59cca6bf38
gsusmoren <jesusmorenogutierrez@gmail.com>
2019-11-24 20:17:02 +0100

    "JFileChooser"
    Funcionando correctamente para cargar una imagen de perfil. Las imagenes
    se cargan en el tamaño escalado a 200px

--- tds_chat/src/main/java/gui/EditProfileWindow.java
+++ tds_chat/src/main/java/gui/EditProfileWindow.java
@@ -10,16 +10,21 @@ import java.awt.KeyboardFocusManager;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.KeyEvent;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
 
 
+import javax.imageio.ImageIO;
 import javax.swing.Box;
 import javax.swing.BoxLayout;
 import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import javax.swing.JDialog;
+import javax.swing.JFileChooser;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JTextField;
+import javax.swing.filechooser.FileNameExtensionFilter;
 
 
 @SuppressWarnings("serial")
@@ -46,7 +51,7 @@ public class EditProfileWindow extends JDialog {
 		Image scaled = im.getScaledInstance(220, 220, java.awt.Image.SCALE_SMOOTH);
 		imagenPerfil = new ImageIcon(scaled);
 
-		JLabel imagenPLabel = new JLabel(imagenPerfil);
+		final JLabel imagenPLabel = new JLabel(imagenPerfil);
 		final JLabel saludoLabel = new JLabel(saludo);
 		imagenPLabel.setAlignmentX(Component.CENTER_ALIGNMENT);
 		saludoLabel.setAlignmentX(Component.CENTER_ALIGNMENT);
@@ -61,11 +66,11 @@ public class EditProfileWindow extends JDialog {
 		editSaludo.setAlignmentX(Component.CENTER_ALIGNMENT);
 		editSaludo.setBorder(Constantes.borderB);
 		editSaludo.setBackground(Color.white);
-		final JDialog aux = this;
+		final JDialog auxJD = this;
 		editSaludo.addActionListener(new ActionListener() {
 
 			public void actionPerformed(ActionEvent e) {
-				final JDialog diagIn = new JDialog(aux, true);
+				final JDialog diagIn = new JDialog(auxJD, true);
 				diagIn.setBounds(Constantes.mainWindow_x * 2 + 40, Constantes.mainWindow_y * 3, 300, 50);
 				final JTextField textS = new JTextField(saludo);
 				diagIn.getContentPane().add(textS);
@@ -79,8 +84,8 @@ public class EditProfileWindow extends JDialog {
 							saludoLabel.setText(saludo);
 
 							diagIn.dispose();
-							aux.validate();
-							aux.repaint();
+							auxJD.revalidate();
+							auxJD.repaint();
 
 							return false;
 						}
@@ -95,6 +100,40 @@ public class EditProfileWindow extends JDialog {
 			}
 		});
 		
+		imagenPLabel.addMouseListener(new MouseAdapter() {
+		
+			public void mouseClicked(MouseEvent e) {
+			
+				JFileChooser fileC = new JFileChooser();
+				fileC.setBounds(Constantes.mainWindow_x,Constantes.mainWindow_y,Constantes.mainWx_size,Constantes.mainWy_size);
+		
+				fileC.addChoosableFileFilter(new FileNameExtensionFilter("Image Files",ImageIO.getReaderFileSuffixes()));
+				fileC.setAcceptAllFileFilterUsed(false);
+				int returnVal = fileC.showOpenDialog(auxJD);
+				
+				if(returnVal == JFileChooser.APPROVE_OPTION) {
+					String pathString =fileC.getCurrentDirectory().toString()+"/";
+					pathString = pathString.concat(fileC.getSelectedFile().getName());
+				
+					//cambiar en la clase padre
+				
+					ImageIcon nImageIcon = new ImageIcon(pathString);
+					
+					Image im = nImageIcon.getImage();
+					Image scaled = im.getScaledInstance(220, 220, java.awt.Image.SCALE_SMOOTH);
+					nImageIcon = new ImageIcon(scaled);
+					
+					imagenPerfil = nImageIcon;
+					imagenPLabel.setIcon(nImageIcon);
+					
+					auxJD.revalidate();
+					auxJD.repaint();
+			
+				}
+			
+			}
+			
+		});
 		
 		
 		
