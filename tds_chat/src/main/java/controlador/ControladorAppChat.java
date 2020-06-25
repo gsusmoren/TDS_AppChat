c9f4dc1ce52518fa940190af56cd788bd0919cab
gsusmoren <jesusmorenogutierrez@gmail.com>
2020-06-25 19:30:57 +0200

    Regist User

--- tds_chat/src/main/java/controlador/ControladorAppChat.java
+++ tds_chat/src/main/java/controlador/ControladorAppChat.java
@@ -1,5 +1,94 @@
 package controlador;
 
+import java.time.LocalDate;
+
+import dao.DAOException;
+import dao.FactoriaDAO;
+import dao.IAdaptadorContactoInidivualDAO;
+import dao.IAdaptadorGrupoDAO;
+import dao.IAdaptadorMensajeDAO;
+import dao.IAdaptadorUsuarioDAO;
+import modelo.CatalogoUsuarios;
+import modelo.Usuario;
+
 public class ControladorAppChat {
 
+	private static ControladorAppChat unicaInstancia;
+
+	private IAdaptadorUsuarioDAO adapU;
+	private IAdaptadorContactoInidivualDAO adapCI;
+	private IAdaptadorGrupoDAO adapGP;
+	private IAdaptadorMensajeDAO adapMS;
+	private CatalogoUsuarios catalogoUsuarios;
+	private Usuario usuarioActual;
+
+	private ControladorAppChat() {
+		inicializarAdaptadores();
+		inicializarCatalogo();
+	}
+
+	public static ControladorAppChat getUnicaInstancia() {
+		if (unicaInstancia == null)
+			unicaInstancia = new ControladorAppChat();
+		return unicaInstancia;
+	}
+
+	// Método para registrar a un usuario en la aplicación
+	public boolean registrarUsuario(String nombre, LocalDate fechaNacimiento, String movil, String email,
+			String contrasena, String nick, String imagen, String saludo) {
+		// Comprobar usuarios unicos
+		if (!isUsuarioUnico(movil, nick)) {
+			return false;
+		}
+
+		Usuario u = new Usuario(nombre, fechaNacimiento, movil, email, contrasena, nick, imagen, saludo);
+		adapU.registrarUsuario(u);
+		catalogoUsuarios.addUsuario(u);
+		return true;
+
+	}
+
+	public boolean isUsuarioUnico(String movil, String nick) {
+		if ((catalogoUsuarios.getUsuario(movil) == null) && (catalogoUsuarios.getUsuario(nick) == null)) {
+			return true;
+		}
+		return false;
+
+	}
+	
+
+	public Usuario getUsuarioActual() {
+
+		return usuarioActual;
+	}
+
+	//Método para logear a un usaurio.....Posibilidad de login con movil
+	public boolean loginUsuario(String login, String passwd) {
+		Usuario usuario = catalogoUsuarios.getUsuario(login);
+		
+		if( usuario != null && usuario.getContrasena().equals(passwd)) {
+			//Establecemos el usuario actual al hacer login
+			this.usuarioActual = usuario;
+			return true;
+		}
+		return false;		
+	}
+	
+	private void inicializarAdaptadores() {
+		FactoriaDAO factoria = null;
+		try {
+			factoria = FactoriaDAO.getInstancia(FactoriaDAO.DAO_TDS);
+
+		} catch (DAOException e) {
+			e.printStackTrace();
+		}
+		adapCI = factoria.getContactoInidivualDAO();
+		adapGP = factoria.getGrupoDAO();
+		adapMS = factoria.getMensajeDAO();
+		adapU = factoria.getUsuarioDAO();
+	}
+
+	private void inicializarCatalogo() {
+		catalogoUsuarios = CatalogoUsuarios.getUnicaInstancia();
+	}
 }
