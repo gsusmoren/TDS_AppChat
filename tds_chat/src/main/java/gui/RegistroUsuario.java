8a7bb4332b70139e767da3fc370147eeff1f2d71
Rubén Ortega <Rubn Ortega@Rubn>
2020-06-25 20:17:27 +0200

    Cancelar de Login

--- tds_chat/src/main/java/gui/RegistroUsuario.java
+++ tds_chat/src/main/java/gui/RegistroUsuario.java
@@ -53,7 +53,7 @@ public class RegistroUsuario extends JPanel {
 		ventana.setResizable(false);
 		this.add(Box.createRigidArea(new Dimension(20, 20)), BorderLayout.NORTH);
 
-		JPanel datos = new JPanel();
+		final JPanel datos = new JPanel();
 		datos.setOpaque(false);
 		this.add(datos, BorderLayout.CENTER);
 		JLabel l = new JLabel("Introduzca sus datos: ");
@@ -197,20 +197,22 @@ public class RegistroUsuario extends JPanel {
 
 		datos.add(btnRegistrar, BorderLayout.SOUTH);
 		datos.add(btnCancelar, BorderLayout.SOUTH);
-
+		
 		btnCancelar.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 				ventana.setContentPane(jpanelAnterior);
-				ventana.dispose();
-				
-
+				ventana.setTitle("AppChat");
+				ventana.getContentPane().revalidate();
+				ventana.getContentPane().repaint();
 			}
 		});
+		
+		
+		ventana.setContentPane(this);
 		this.revalidate();
 		this.repaint();
-		ventana.add(this);
 		ventana.setVisible(true);
-
+		
 	}
 
 }
